f7632180ce961e9847b4e61c90397ab9c199b2fc
dennis <dennis.michurin@gmail.com>
2024-01-30 22:14:20 +0300

    lox

--- demo/src/main/java/com/example/demo/BlockApplication.java
+++ demo/src/main/java/com/example/demo/BlockApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlockApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlockApplication.class, args);
 	}
